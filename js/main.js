31705654274b81e16903169fa5ec9ef670411a10
JuanPabloCardenasGonzalez <jpcardenasgonzalez@gmail.com>
2024-01-18 14:07:59 -0500

    Mejora de css a mobile first y se ageregó el botón de menú

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 AOS.init({
     offset:1
 });
+
+
+    let links = document.querySelector('.links');
+    let buttonMenu = document.querySelector('.button-menu');
+
+    buttonMenu.addEventListener('click', () => {
+        links.classList.toggle('active');
+    });
